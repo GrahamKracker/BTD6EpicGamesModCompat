82d148fcb0e791a813770f3518f6ac2cfdd9fd37
GrahamKracker <gjguzynski@gmail.com>
2023-08-07 23:45:10 -0400

    maybe now it'll stop complaining

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,10 +2,10 @@ namespace BTD6EpicGamesModCompat;
 
 public static class ModHelperData
 {
-    public const string Name = "BTD6 Epic Games Mod Compat";
-    public const string Description = "Retargets mods from the steam version to work on the Epic Games version of BTD6.";
-    public const string Version = "1.2.0";
-    public const string RepoOwner = "GrahamKracker";
-    public const string RepoName = "BTD6EpicGamesModCompat";
-    public const string WorksOnVersion = "38.2";
+    internal const string Name = "BTD6 Epic Games Mod Compat";
+    internal const string Description = "Retargets mods from the steam version to work on the Epic Games version of BTD6.";
+    internal const string Version = "1.2.0";
+    internal const string RepoOwner = "GrahamKracker";
+    internal const string RepoName = "BTD6EpicGamesModCompat";
+    internal const string WorksOnVersion = "38.2";
 }
